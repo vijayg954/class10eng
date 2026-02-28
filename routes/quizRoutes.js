3db40dd5e4cdd5867b8cc1b8b1b326fa8417e091
vijay <vijayg954@gmail.com>
2026-02-28 11:58:11 +0530

    first commit

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.post("/new", createQuiz);
 router.get("/", getQuizList);
 router.get("/:id", getQuizById);
-router.put("/quiz/:id", updateQuiz);
+router.put("/:id", updateQuiz);
 export default router;
 
 
